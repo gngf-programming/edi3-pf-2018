5a913b83f5dacf7ef72cf2300644edd7ecae557f
Gabriel Ferreira <gabrielferreira@ecolan.edu.ar>
2018-10-20 07:36:27 -0300

    corrección a DatabaseHandlerInterface

--- include/compset/DatabaseHandlerInterface.h
+++ include/compset/DatabaseHandlerInterface.h
@@ -20,21 +20,21 @@ typedef std::unordered_map<std::string, std::string> DatumType;
 class DatabaseHandler
 {
     public:
-    	virtual ~DatabaseHandler(){}
-		virtual bool getErrorStatus() = 0;
-		virtual bool getResultStatus() = 0;
-		virtual DatabaseHandler* setQuery( std::string query ) = 0;
-		virtual DatabaseHandler* setStoredProcedure( std::string storedProcedure ) = 0;
-		virtual DatabaseHandler* withResultStatus() = 0;
-		virtual DatabaseHandler* addParameter( std::string key, std::string value ) = 0;
-		virtual void execQuery() = 0;
-		virtual DataType fetch() = 0;
-		virtual DatumType fetchAll() = 0;
-		
-		//virtual json execute();//En un futuro....
-		//json
-		//unordered_map<std::string, std::string> = 0;
-		//virtual void dispose(); //Ver si realmente tiene que ser explícito 
+        virtual ~DatabaseHandler(){}
+        virtual bool getErrorStatus() = 0;
+        virtual bool getResultStatus() = 0;
+        virtual DatabaseHandler* setQuery( std::string query ) = 0;
+        virtual DatabaseHandler* setStoredProcedure( std::string storedProcedure ) = 0;
+        virtual DatabaseHandler* withResultStatus() = 0;
+        virtual DatabaseHandler* addParameter( std::string key, std::string value ) = 0;
+        virtual void execQuery() = 0;
+        virtual DataType fetch() = 0;
+        virtual DatumType fetchAll() = 0;
+        
+        //virtual json execute();//En un futuro....
+        //json
+        //unordered_map<std::string, std::string> = 0;
+        //virtual void dispose(); //Ver si realmente tiene que ser explícito 
 };
 
 #endif // DATABASE_HANLDER_INTERFACE_H
