ef08b7fd6ebdabee413b6e3a628c900462064898
Giuliano <Giulianodecesares@gmail.com>
2018-10-29 05:21:54 -0300

    Test complete rework (still working on c++ regex)

--- src/XmlDocumentGenerator/test/test.cpp
+++ src/XmlDocumentGenerator/test/test.cpp
@@ -1,47 +1,103 @@
-/**
-* Copyright (c) 2016 Gabriel Ferreira <gabrielinuz@gmail.com>. All rights reserved. 
-* This file is part of COMPSET.
-* Released under the GPL3 license
-* https://opensource.org/licenses/GPL-3.0
-**/
-
-#include <vector>
-#include <unordered_map>
 #include <string>
 #include <iostream>
 
-#include <compset/ComponentFactory.h>
-#include <compset/ComponentInterface.h>
-#include <compset/DocumentGeneratorInterface.h>
+#include <assert.h>
+#include <regex>
 
-typedef std::vector< std::unordered_map<std::string, std::string> > DataType; 
-typedef std::unordered_map<std::string, std::string> DatumType; 
+#include <compset/DocumentGeneratorInterface.h>
+#include <compset/Component.h>
+
+DataType GenerateTestXMLContent(void);
+
+bool CheckXMLContent(std::string xmlContent);
+
+void PrintXMLContent(Component<DocumentGeneratorInterface>& xmlGenerator);
+void XMLGeneratorPlayground(Component<DocumentGeneratorInterface>& xmlGenerator);
 
 int main()
 {
-    DataType data;
-    DatumType datum;
+	// 1 - Component creation (RAII approves)
+	Component<DocumentGeneratorInterface> xmlGenerator("DocumentGeneratorInterface", "./XmlDocumentGenerator");
 
-    datum["last_name"] = "Doe";
-    datum["first_name"] = "John";
-    data.push_back(datum);
+	// 2 - XML generator test method
+	XMLGeneratorPlayground(xmlGenerator);
+	
+	return 0;
+}
 
-    datum["last_name"] = "Smith";
-    datum["first_name"] = "Ana";
-    data.push_back(datum);
-    
-    datum["last_name"] = "Jones";
-    datum["first_name"] = "Peter";
-    data.push_back(datum);
+DataType GenerateTestXMLContent(void)
+{
+	DataType returnContent;
+	DatumType fieldContent;
 
-    ComponentFactory* componentFactoryObject = new ComponentFactory();
-    componentFactoryObject->setInterfaceName("DocumentGeneratorInterface");
-    ComponentInterface* documentGeneratorComponent = componentFactoryObject->createFrom("../XmlDocumentGenerator");
-    delete componentFactoryObject;
+	fieldContent["last_name"] = "Doe";
 
-    DocumentGeneratorInterface* documentGeneratorObject = ( (DocumentGeneratorInterface*) documentGeneratorComponent->getInstance() );
-    documentGeneratorObject->setContent(data);
-    std::cout << "Test Document: " << documentGeneratorObject->getDocument() << std::endl;
-    documentGeneratorComponent->release();
-    return 0;
+	returnContent.push_back(fieldContent);
+
+	return returnContent;
+}
+
+bool CheckXMLContent(std::string xmlContent)
+{
+	/*
+	Content - type: text / xml; charset = utf - 8
+
+	<?xml version = "1.0" encoding = "UTF-8"?>
+
+	<data>
+		<datum>
+			<last_name>Doe<last_name>
+		</datum>
+	</data>
+	*/
+
+	try {
+		// Any string that starts with <? then anything less than ?> and then ?>
+		const std::regex xmlHeaderLabel("\\<\\?([^\\?|^\\>]*)\\?\\>");
+
+		// Any string that starts with </ then anything with letters, numbers or underscore, and then >
+		const std::regex xmlClosingLabel("\\<(/[a-zA-Z_0-9]*)\\>");
+
+		// Any string that starts with < then anything with letters, numbers or underscore, and then >
+		const std::regex xmlOpeningLabel("\\<([a-zA-Z_0-9]*)\\>");
+
+		std::sregex_iterator next(xmlContent.begin(), xmlContent.end(), xmlHeaderLabel);
+		std::sregex_iterator end;
+
+		while (next != end) 
+		{
+			std::smatch match = *next;
+
+			std::cout << match.str() << "\n";
+
+			next++;
+		}
+	}
+	catch (std::regex_error& e) 
+	{
+		std::cout << "Syntax error in the regular expression" << std::endl << std::endl;
+	}
+
+	return false;
+}
+
+
+void XMLGeneratorPlayground(Component<DocumentGeneratorInterface>& xmlGenerator)
+{
+	// XML data creation for testing proposes
+	DataType data = GenerateTestXMLContent();
+
+	// XML document generator content set
+	xmlGenerator->setContent(data);
+
+	// XML document generator content print
+	// PrintXMLContent(xmlGenerator);
+
+	// XML document generator content check
+	CheckXMLContent(xmlGenerator->getDocument());
+}
+
+void PrintXMLContent(Component<DocumentGeneratorInterface>& xmlGenerator)
+{
+	std::cout << xmlGenerator->getDocument() << std::endl;
 }
