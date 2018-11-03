3cb589d5e52b361413ff9bc7b1e91d117ee85c52
Facundo Nicolás Campos <facundo.nicolas.campos@gmail.com>
2018-11-03 02:41:14 -0300

    depuring files 2

--- src/Actions/DBGeneratorMariaDB/build.bat
+++ src/Actions/DBGeneratorMariaDB/build.bat
@@ -1,2 +1,2 @@
-g++ -shared -Wall -std=c++11 DBGeneratorMariaDB.cpp -o DBGeneratorMariaDB.dll -I./include -I../../../include/compset vendors\libmysql.dll
+g++ -shared -Wall -std=c++11 DBGeneratorMariaDB.cpp -o lib\DBGeneratorMariaDB.dll -I./include -I../../../include/compset vendors\libmysql.dll
 g++ -Wall -std=c++11 test.cpp include\DBGeneratorMariaDB.h -o test.exe -I./include -I../../../include/compset vendors\libmysql.dll lib\DBGeneratorMariaDB.dll
