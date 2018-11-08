40407e468054d1272b910100cc65efe97e2553b4
“Hernán <hernan_amarillo@live.com.ar>
2018-11-07 23:16:50 -0300

    Base code for testing PostgreSQLDatabaseHandler

---
+++ src/DatabaseHandler/test/test.cpp
@@ -0,0 +1,27 @@
+/**
+* Copyright (c) 2016 Gabriel Ferreira <gabrielinuz@gmail.com>. All rights reserved. 
+* This file is part of COMPSET.
+* Released under the GPL3 license
+* https://opensource.org/licenses/GPL-3.0
+**/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+#include <compset/ComponentInterface.h>
+#include <compset/ComponentFactory.h>
+#include <compset/DatabaseHandlerInterface.h>
+
+int main()
+{
+    ComponentFactory* componentFactoryObject = new ComponentFactory();
+    componentFactoryObject->setInterfaceName("DatabaseHandlerInterface");
+    ComponentInterface* databaseHandlerComponent = componentFactoryObject->createFrom("../PostgreSQLDatabaseHandler");
+    DatabaseHandlerInterface* databaseHandlerObject = ( (DatabaseHandlerInterface*) databaseHandlerComponent->getInstance() );
+    DataType allData = databaseHandlerObject->fetchAll();
+    delete componentFactoryObject;
+
+    return 0;
+}
