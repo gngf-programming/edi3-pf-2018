c4b3db2c979c2e225beffa408c7a84953553e212
Nano Nobile <crlsdnlnobile@gmail.com>
2018-11-08 21:42:31 -0300

    add bin directory

--- src/mariadb/build_dll.bat
+++ src/mariadb/build_dll.bat
@@ -1,1 +1,1 @@
-g++ -shared -Wall -std=c++11  -m32 -I../../include/ libmariadb.dll mdb.cpp -o mdb-app.dll
+g++ -shared -Wall -std=c++11  -m32 -I../../include/ libmariadb.dll mdb.cpp -o ./bin/mdb-app.dll
