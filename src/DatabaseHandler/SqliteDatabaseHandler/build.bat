4fdf91c8c238f1f8c861c2bcf4c50a74e86c8bf0
diedarisu <diedarisu@yahoo.com.ar>
2018-11-15 19:41:30 -0200

    SqliteDatabaseHandler Tests start

---
+++ src/DatabaseHandler/SqliteDatabaseHandler/build.bat
@@ -0,0 +1,1 @@
+g++ -shared -Wall -std=c++11 -I../../../include/ -I../../../include/vendors/sqlite/ -o SqliteDatabaseHandler.dll SqliteDatabaseHandler.cpp ../../../include/vendors/sqlite/sqlite3.dll
